be057b05cbce0a6c7960ef2613fc377f63be4c0b
scryotic-tech <81018435+scryotic-tech@users.noreply.github.com>
2022-11-09 08:15:54 +0000

    Update exorde.sh

--- exorde.sh
+++ exorde.sh
@@ -30,7 +30,7 @@ echo -e "\e[1m\e[32m2. Installing Screen... \e[0m" && sleep 1
 sudo apt install -y build-essential libssl-dev libffi-dev git curl screen
 
 echo -e "\e[1m\e[32m2. cloning github... \e[0m" && sleep 1
-git clone https://github.com/exorde-labs/ExordeModuleCLI.git
+gh repo clone exorde-labs/ExordeModuleCLI
 
 echo -e "\e[1m\e[32m2. Building exorde... \e[0m" && sleep 1
 cd ExordeModuleCLI
